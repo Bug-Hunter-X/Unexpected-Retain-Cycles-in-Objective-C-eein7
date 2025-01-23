63c33b43e5218ffd0a64a77ff327bd9a9705c202
BugHunter-X <askzenai@gmail.com>
2025-01-23 08:30:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,10 @@
+In Objective-C, a tricky error can occur when dealing with memory management and property declarations.  Consider this scenario:
+
+```objectivec
+@interface MyClass : NSObject
+@property (strong, nonatomic) NSString *myString;
+@end
+
+@implementation MyClass
+- (void)someMethod {
+    self.myString = [[NSString alloc] initWithString:@
